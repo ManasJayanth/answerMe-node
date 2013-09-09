8c89d6cc4dc7351ce2fe84516d3bf5ca1c353ff3
Manas <harlequinsguitar@gmail.com>
2013-09-10 00:00:19 +0530

    Base files created.

---
+++ app.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var static = require('node-static');
+var file = new static.Server();
+var url = require('url');
+var index = require('./njs/index.js');
+var login = require('./njs/login.js');
+var ajax = require('./njs/ajax.js');
+var eval = require('./njs/eval.js');
+var getquestion = require('./njs/getquestion.js');
+var initserv = require('./njs/initserv.js');
+var serverspecific = require('./njs/serverspecific.js');
+var taketest = require('./njs/taketest.js');
+var usernameajax = require('./njs/usernameajax.js');
+
+http.createServer(function (req, res) {
+	if (url.parse(req.url).pathname == '/index/') {
+		index.serve(req, res);
+	}
+	else if (url.parse(req.url).pathname == '/') {
+		index.serve(req, res);
+	} 
+	else if (url.parse(req.url).pathname == '/njs/login/') {
+		login.serve(req, res);
+	} 
+	else if (url.parse(req.url).pathname == '/njs/ajax/') {
+		ajax.serve(req, res);
+	} 
+	else if (url.parse(req.url).pathname == '/njs/eval/') {
+		eval.serve(req, res);
+	}
+	else if (url.parse(req.url).pathname == '/njs/getquestion/') {
+		getquestion.serve(req, res);
+	} 
+	else if (url.parse(req.url).pathname == '/njs/initserv/') {
+		initserv.serve(req, res);
+	}
+	else if (url.parse(req.url).pathname == '/njs/serverspecific/') {
+		serverspecific.serve(req, res);
+	}
+	else if (url.parse(req.url).pathname == '/njs/taketest/') {
+		taketest.serve(req, res);
+	}
+	else if (url.parse(req.url).pathname == '/njs/usernameajax/') {
+		usernameajax.serve(req, res);
+	} 
+	else {
+		file.serve(req, res);
+	}
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
---
+++ njs/ajax.js
@@ -0,0 +1,4 @@
+exports.serve = function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('ajax.js: work in progress!');
+};
